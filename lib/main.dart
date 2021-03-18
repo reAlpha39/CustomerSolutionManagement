cba900091bd4333984eaa779cb51688a65cfc43e
ReAlpha39 <yunusw718@gmail.com>
2021-03-18 12:35:04 +0800

    disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         getPages: Routes.route,
         initialRoute: '/login_page',

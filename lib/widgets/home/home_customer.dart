571574f6e1fc80e4aa8141661c15b3405cf99fe3
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2021-03-25 20:51:48 +0700

    add card icon menu

--- lib/widgets/home/home_customer.dart
+++ lib/widgets/home/home_customer.dart
@@ -1,12 +1,17 @@
+import 'package:customer/widgets/card_icon_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeCustomer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ElevatedButton(
-            child: Text('Need support United Tractors'),
-            onPressed: () => Get.toNamed('/customer_info')));
+    return Column(
+      children: [
+        Container(
+            child: ElevatedButton(
+                child: Text('Need support United Tractors'),
+                onPressed: () => Get.toNamed('/customer_info'))),
+      ],
+    );
   }
 }

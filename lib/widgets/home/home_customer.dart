1f57e20a3b6579ae27ae3e7b78772f4a2f0ffbe9
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2021-03-25 23:47:44 +0700

    add card to home customer

--- lib/widgets/home/home_customer.dart
+++ lib/widgets/home/home_customer.dart
@@ -1,4 +1,3 @@
-import 'package:customer/widgets/card_icon_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,10 +6,63 @@ class HomeCustomer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-            child: ElevatedButton(
-                child: Text('Need support United Tractors'),
-                onPressed: () => Get.toNamed('/customer_info'))),
+        Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                child: ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    onTap: () => Get.toNamed('/customer_info'),
+                    trailing: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(maxHeight: 100, maxWidth: 100),
+                      child: Image.asset('assets/images/need_support.png'),
+                    ),
+                    title: Text("Need Support UT")))),
+        Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                child: ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    trailing: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(maxHeight: 100, maxWidth: 100),
+                      child: Image.asset('assets/images/service_program.png'),
+                    ),
+                    title: Text("Service Program")))),
+        Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                child: ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    trailing: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(maxHeight: 100, maxWidth: 100),
+                      child: Image.asset('assets/images/part_program.png'),
+                    ),
+                    title: Text("Part Program")))),
+        Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                child: ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    trailing: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(maxHeight: 100, maxWidth: 100),
+                      child: Image.asset('assets/images/other_program.png'),
+                    ),
+                    title: Text("Other Program")))),
       ],
     );
   }

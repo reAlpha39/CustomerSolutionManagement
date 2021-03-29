4437793f01b735ad1c1946f1d2a994ca8917199c
ReAlpha39 <yunusw718@gmail.com>
2021-03-29 14:25:59 +0800

    add background color

--- lib/widgets/home/home_card_expanded.dart
+++ lib/widgets/home/home_card_expanded.dart
@@ -46,117 +46,130 @@ class HomeCardExpanded extends StatelessWidget {
                         collapsed: Container(),
                         expanded: Padding(
                           padding: const EdgeInsets.only(top: 10),
-                          child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 10, left: 30, right: 30, bottom: 10),
-                                  child: ElevatedButton(
-                                    onPressed: () => Get.toNamed('/mspp'),
-                                    style: ButtonStyle(
-                                        elevation:
-                                            MaterialStateProperty.all<double>(
-                                                0),
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        17)))),
-                                    child: Container(
-                                      margin: EdgeInsets.only(
-                                          top: 20,
-                                          bottom: 20,
-                                          left: 10,
-                                          right: 10),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Center(
-                                            child: Text(
-                                              'MSPP',
-                                              style: TextStyle(fontSize: 18),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey.shade100,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(17))),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10,
+                                        bottom: 10,
+                                        right: 20,
+                                        left: 20),
+                                    child: ElevatedButton(
+                                      onPressed: () => Get.toNamed('/mspp'),
+                                      style: ButtonStyle(
+                                          elevation:
+                                              MaterialStateProperty.all<double>(
+                                                  0),
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          17)))),
+                                      child: Container(
+                                        margin: EdgeInsets.only(
+                                            top: 20,
+                                            bottom: 20,
+                                            left: 10,
+                                            right: 10),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Center(
+                                              child: Text(
+                                                'MSPP',
+                                                style: TextStyle(fontSize: 18),
+                                              ),
                                             ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5, bottom: 5),
-                                            child: Container(
-                                              height: 1,
-                                              color: Colors.black,
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 5, bottom: 5),
+                                              child: Container(
+                                                height: 1,
+                                                color: Colors.black,
+                                              ),
                                             ),
-                                          ),
-                                          Center(
-                                            child: Text(
-                                                'Maintenance & Support Partnership Program',
+                                            Center(
+                                              child: Text(
+                                                  'Maintenance & Support Partnership Program',
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.black87),
+                                                  textAlign: TextAlign.center),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10,
+                                        bottom: 10,
+                                        left: 20,
+                                        right: 20),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                          elevation:
+                                              MaterialStateProperty.all<double>(
+                                                  0),
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          17)))),
+                                      child: Container(
+                                        margin: EdgeInsets.only(
+                                            top: 20,
+                                            bottom: 20,
+                                            left: 10,
+                                            right: 10),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Center(
+                                              child: Text(
+                                                'CMIP & COIP',
+                                                style: TextStyle(fontSize: 18),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 5, bottom: 5),
+                                              child: Container(
+                                                height: 1,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Center(
+                                              child: Text(
+                                                'Maintenance & Operation Program',
                                                 style: TextStyle(
                                                     fontSize: 12,
                                                     color: Colors.black87),
-                                                textAlign: TextAlign.center),
-                                          ),
-                                        ],
+                                                textAlign: TextAlign.center,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 10, left: 30, right: 30, bottom: 10),
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ButtonStyle(
-                                        elevation:
-                                            MaterialStateProperty.all<double>(
-                                                0),
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        17)))),
-                                    child: Container(
-                                      margin: EdgeInsets.only(
-                                          top: 20,
-                                          bottom: 20,
-                                          left: 10,
-                                          right: 10),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Center(
-                                            child: Text(
-                                              'CMIP & COIP',
-                                              style: TextStyle(fontSize: 18),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5, bottom: 5),
-                                            child: Container(
-                                              height: 1,
-                                              color: Colors.black,
-                                            ),
-                                          ),
-                                          Center(
-                                            child: Text(
-                                              'Maintenance & Operation Program',
-                                              style: TextStyle(
-                                                  fontSize: 12,
-                                                  color: Colors.black87),
-                                              textAlign: TextAlign.center,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ]),
+                                ]),
+                          ),
                         )),
                   ),
                 ))));

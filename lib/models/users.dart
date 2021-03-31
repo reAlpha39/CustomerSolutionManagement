fcf6eec4e29db46f1a3d66a4a417c864514a3b26
ReAlpha39 <yunusw718@gmail.com>
2021-03-31 15:26:15 +0800

    add new object

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 
 class Users {
     Users({
+        this.nama,
         this.username,
         this.password,
         this.type,
     });
 
+    String nama;
     String username;
     String password;
     String type;
@@ -16,12 +18,14 @@ class Users {
     String toJson() => json.encode(toMap());
 
     factory Users.fromMap(Map<String, dynamic> json) => Users(
+        nama: json["nama"],
         username: json["username"],
         password: json["password"],
         type: json["type"],
     );
 
     Map<String, dynamic> toMap() => {
+        "nama": nama,
         "username": username,
         "password": password,
         "type": type,

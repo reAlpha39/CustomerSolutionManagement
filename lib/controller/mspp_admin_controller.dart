982bedc2ed04ec9750701d99f21688b95e7d21f0
ReAlpha39 <yunusw718@gmail.com>
2021-04-02 15:40:06 +0800

    add button to choose user

--- lib/controller/mspp_admin_controller.dart
+++ lib/controller/mspp_admin_controller.dart
@@ -1,16 +1,36 @@
+import 'package:customer/controller/login_controller.dart';
 import 'package:customer/repositories/database_provider.dart';
 import 'package:get/get.dart';
 
 class MsppAdminController extends GetxController {
   final DatabaseProvider databaseProvider = DatabaseProvider();
 
-  RxList<String> listCustomer = [""].obs;
+  RxList<String> listCustomer = [''].obs;
+  RxString idCustomer = ''.obs;
+  RxBool isLoading = false.obs;
 
   @override
   void onInit() {
-    databaseProvider
-        .listCustomer()
-        .then((value) => listCustomer.assignAll(value));
+    loadListCustomer();
     super.onInit();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  loadListCustomer() {
+    try {
+      isLoading.value = true;
+      print(isLoading.value);
+      databaseProvider.listCustomer().then((value) {
+        print(value);
+        listCustomer.addAll(value);
+        listCustomer.removeAt(0);
+        isLoading.value = false;
+        print(isLoading.value);
+      });
+    } catch (Exception) {}
   }
 }
--- lib/views/mspp_admin.dart
+++ lib/views/mspp_admin.dart
@@ -1,13 +1,64 @@
+import 'package:customer/controller/mspp_admin_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MsppAdmin extends StatelessWidget {
+  final MsppAdminController controller = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MSPP'),
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Obx(
+            () => Text('User: ' + controller.idCustomer.value),
+          ),
+        ),
+      ),
+      floatingActionButton: Obx(
+        () => FloatingActionButton(
+          child: controller.isLoading.value
+              ? CircularProgressIndicator()
+              : Icon(Icons.person_outline_sharp),
+          onPressed: listCustomer,
+        ),
+      ),
+    );
+  }
+
+  Future<bool> listCustomer() {
+    return Get.defaultDialog(
+        barrierDismissible: false,
+        radius: 17,
+        title: 'Pilih salah satu',
+        content: ListCustomer(),
+        textConfirm: 'OK',
+        buttonColor: Color(0xffffcd29),
+        confirmTextColor: Colors.black87,
+        onConfirm: () => Get.back(closeOverlays: false));
+  }
+}
+
+class ListCustomer extends StatelessWidget {
+  final MsppAdminController controller = Get.find();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        height: 200,
+        child: ListView.builder(
+          itemCount: controller.listCustomer.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${controller.listCustomer[index]}'),
+              onTap: () =>
+                  controller.idCustomer.value = controller.listCustomer[index],
+            );
+          },
+        ),
+      ),
     );
   }
 }

6817325a79329e912ee1609c47ae945c3f203d22
ReAlpha39 <yunusw718@gmail.com>
2021-04-05 14:48:56 +0800

    add more properties on mspp object

--- lib/models/mspp.dart
+++ lib/models/mspp.dart
@@ -11,11 +11,19 @@ class Mspp {
     this.periodicInspection,
     this.periodicServicePlan,
     this.periodicService,
+    this.tools,
+    this.infrastructur,
+    this.ketersediaanMekanik,
+    this.leadTimePenyelesaianP,
   });
 
   Map<String, MsppData> periodicInspection;
   Map<String, MsppData> periodicServicePlan;
   Map<String, MsppData> periodicService;
+  Map<String, MsppData> tools;
+  Map<String, MsppData> infrastructur;
+  Map<String, MsppData> ketersediaanMekanik;
+  Map<String, MsppData> leadTimePenyelesaianP;
 
   factory Mspp.fromJson(String str) => Mspp.fromMap(json.decode(str));
 
@@ -28,6 +36,14 @@ class Mspp {
             .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
         periodicService: Map.from(json["periodicService"])
             .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        tools: Map.from(json["tools"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        infrastructur: Map.from(json["infrastructure"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        ketersediaanMekanik: Map.from(json["ketersediaanMekanik"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        leadTimePenyelesaianP: Map.from(json["leadTimePenyelesaianP"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
       );
 
   Map<String, dynamic> toMap() => {
@@ -36,6 +52,14 @@ class Mspp {
         "periodicServicePlan": Map.from(periodicServicePlan)
             .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
         "periodicService": Map.from(periodicService)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "tools": Map.from(tools)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "infrastructure": Map.from(infrastructur)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "ketersediaanMekanik": Map.from(ketersediaanMekanik)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "leadTimePenyelesaianP": Map.from(leadTimePenyelesaianP)
             .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
       };
 }

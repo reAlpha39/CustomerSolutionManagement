fa39e8ade6b8ce2ecd1065ccaad1f2a4f7a0e773
ReAlpha39 <yunusw718@gmail.com>
2021-04-05 16:03:15 +0800

    save new properties data to db

--- lib/controller/mspp_controller.dart
+++ lib/controller/mspp_controller.dart
@@ -3,6 +3,7 @@ import 'package:customer/models/mspp.dart';
 import 'package:customer/models/mspp_data.dart';
 import 'package:customer/repositories/database_provider.dart';
 import 'package:customer/utils/connectivity_checker.dart';
+import 'package:customer/widgets/mspp/mspp_card/mspp_card_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -32,9 +33,7 @@ class MsppController extends GetxController {
   RxList<int> radioIndexMTDT = [-1].obs;
   RxList<int> radioIndexFIELD =
       [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1].obs;
-  RxList<int> radioIndexWORKSHOP =
-      [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1].obs;
+  RxList<int> radioIndexWORKSHOP = RxList.filled(26, -1);
   RxList<int> radioIndexKM = [-1, -1, -1, -1].obs;
   RxList<int> radioIndexLTPP = [-1, -1, -1, -1].obs;
 
@@ -59,9 +58,7 @@ class MsppController extends GetxController {
   RxList<String> textFieldMTDT = [""].obs;
   RxList<String> textFieldFIELD =
       ["", "", "", "", "", "", "", "", "", "", "", "", "", ""].obs;
-  RxList<String> textFieldWORKSHOP =
-      ["", "", "", "", "", "", "", "", "", "", "", "", "",
-       "", "", "", "", "", "", "", "", "", "", "", "", ""].obs;
+  RxList<String> textFieldWORKSHOP = RxList.filled(26, "");
   RxList<String> textFieldKM = ["", "", "", ""].obs;
   RxList<String> textFieldLTPP = ["", "", "", ""].obs;
 
@@ -125,6 +122,18 @@ class MsppController extends GetxController {
                   .assignAll(value.periodicService['eds'].assessmentResult);
               radioIndexRPLL
                   .assignAll(value.periodicService['rpll'].assessmentResult);
+              radioIndexCTST
+                  .assignAll(value.periodicService['ctst'].assessmentResult);
+              radioIndexMTDT
+                  .assignAll(value.periodicService['mtdt'].assessmentResult);
+              radioIndexFIELD
+                  .assignAll(value.periodicService['field'].assessmentResult);
+              radioIndexWORKSHOP.assignAll(
+                  value.periodicService['workshop'].assessmentResult);
+              radioIndexKM
+                  .assignAll(value.periodicService['km'].assessmentResult);
+              radioIndexLTPP
+                  .assignAll(value.periodicService['ltpp'].assessmentResult);
               textFieldPU
                   .assignAll(value.periodicInspection['planUnit'].remark);
               textFieldMeet.assignAll(value.periodicInspection['meet'].remark);
@@ -145,6 +154,13 @@ class MsppController extends GetxController {
               textFieldHPTD.assignAll(value.periodicService['hptd'].remark);
               textFieldEDS.assignAll(value.periodicService['eds'].remark);
               textFieldRPLL.assignAll(value.periodicService['rpll'].remark);
+              textFieldCTST.assignAll(value.periodicService['ctst'].remark);
+              textFieldMTDT.assignAll(value.periodicService['mtdt'].remark);
+              textFieldFIELD.assignAll(value.periodicService['field'].remark);
+              textFieldWORKSHOP
+                  .assignAll(value.periodicService['workshop'].remark);
+              textFieldKM.assignAll(value.periodicService['km'].remark);
+              textFieldLTPP.assignAll(value.periodicService['ltpp'].remark);
             }
           },
         );
@@ -187,6 +203,19 @@ class MsppController extends GetxController {
           MsppData(assessmentResult: radioIndexEDS, remark: textFieldEDS);
       MsppData msppDataRPLL =
           MsppData(assessmentResult: radioIndexRPLL, remark: textFieldRPLL);
+      MsppData msppDataCTST =
+          MsppData(assessmentResult: radioIndexCTST, remark: textFieldCTST);
+      MsppData msppDataMTDT =
+          MsppData(assessmentResult: radioIndexMTDT, remark: textFieldMTDT);
+      MsppData msppDataFIELD =
+          MsppData(assessmentResult: radioIndexFIELD, remark: textFieldFIELD);
+      MsppData msppDataWORKSHOP = MsppData(
+          assessmentResult: radioIndexWORKSHOP, remark: textFieldWORKSHOP);
+      MsppData msppDataKM =
+          MsppData(assessmentResult: radioIndexKM, remark: textFieldKM);
+      MsppData msppDataLTPP =
+          MsppData(assessmentResult: radioIndexLTPP, remark: textFieldLTPP);
+
       Mspp mspp = Mspp(periodicInspection: {
         'planUnit': msppDataPU,
         'meet': msppDataMeet,
@@ -205,6 +234,16 @@ class MsppController extends GetxController {
         'hptd': msppDataHPTD,
         'eds': msppDataEDS,
         'rpll': msppDataRPLL
+      }, tools: {
+        'ctst': msppDataCTST,
+        'mtdt': msppDataMTDT
+      }, infrastructur: {
+        'field': msppDataFIELD,
+        'workshop': msppDataWORKSHOP
+      }, ketersediaanMekanik: {
+        'km': msppDataKM
+      }, leadTimePenyelesaianP: {
+        'ltpp': msppDataLTPP
       });
       _databaseProvider.saveMSPP(mspp, _loginController.usr.value.username);
     });
@@ -264,6 +303,24 @@ class MsppController extends GetxController {
       case 'rpll':
         data = textFieldRPLL;
         break;
+      case 'ctst':
+        data = textFieldCTST;
+        break;
+      case 'mtdt':
+        data = textFieldMTDT;
+        break;
+      case 'field':
+        data = textFieldFIELD;
+        break;
+      case 'workshop':
+        data = textFieldWORKSHOP;
+        break;
+      case 'km':
+        data = textFieldKM;
+        break;
+      case 'ltpp':
+        data = textFieldLTPP;
+        break;
       default:
     }
     return data;
@@ -316,6 +373,24 @@ class MsppController extends GetxController {
         break;
       case 'rpll':
         data = radioIndexRPLL;
+        break;
+      case 'ctst':
+        data = radioIndexCTST;
+        break;
+      case 'mtdt':
+        data = radioIndexMTDT;
+        break;
+      case 'field':
+        data = radioIndexFIELD;
+        break;
+      case 'workshop':
+        data = radioIndexWORKSHOP;
+        break;
+      case 'km':
+        data = radioIndexKM;
+        break;
+      case 'ltpp':
+        data = radioIndexLTPP;
         break;
       default:
     }

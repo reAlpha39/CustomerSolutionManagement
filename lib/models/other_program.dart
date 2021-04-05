c5890e059e82ebaa6755a30fac841db8b5a079bd
ReAlpha39 <yunusw718@gmail.com>
2021-04-05 19:52:56 +0800

    change model properties

--- lib/models/other_program.dart
+++ lib/models/other_program.dart
@@ -7,29 +7,36 @@ import 'dart:convert';
 import 'mspp_data.dart';
 
 class OtherProgram {
-    OtherProgram({
-        this.people,
-        this.vm,
-        this.regM,
-    });
+  OtherProgram({
+    this.people,
+    this.vm,
+    this.regM,
+  });
 
-    MsppData people;
-    MsppData vm;
-    MsppData regM;
+  Map<String, MsppData> people;
+  Map<String, MsppData> vm;
+  Map<String, MsppData> regM;
 
-    factory OtherProgram.fromJson(String str) => OtherProgram.fromMap(json.decode(str));
+  factory OtherProgram.fromJson(String str) =>
+      OtherProgram.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory OtherProgram.fromMap(Map<String, dynamic> json) => OtherProgram(
-        people: MsppData.fromMap(json["people"]),
-        vm: MsppData.fromMap(json["vm"]),
-        regM: MsppData.fromMap(json["regM"]),
-    );
+  factory OtherProgram.fromMap(Map<String, dynamic> json) => OtherProgram(
+        people: Map.from(json["people"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        vm: Map.from(json["vm"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+        regM: Map.from(json["regM"])
+            .map((k, v) => MapEntry<String, MsppData>(k, MsppData.fromMap(v))),
+      );
 
-    Map<String, dynamic> toMap() => {
-        "people": people.toMap(),
-        "vm": vm.toMap(),
-        "regM": regM.toMap(),
-    };
+  Map<String, dynamic> toMap() => {
+        "people": Map.from(people)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "vm":
+            Map.from(vm).map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+        "regM": Map.from(regM)
+            .map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
+      };
 }

cdd31ea2d07b655cb7fc29dcbc4094a5bf8527b0
ReAlpha39 <yunusw718@gmail.com>
2021-04-07 14:05:41 +0800

    add other program page

---
+++ lib/routes/other_program_admin_binding.dart
@@ -0,0 +1,9 @@
+import 'package:customer/controller/other_program_admin_controller.dart';
+import 'package:get/get.dart';
+
+class OtherProgramAdminBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<OtherProgramAdminController>(() => OtherProgramAdminController());
+  }
+}

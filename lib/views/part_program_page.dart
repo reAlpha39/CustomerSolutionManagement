b7fded8286879615b13715cce6176a2b9d66d671
ReAlpha39 <yunusw718@gmail.com>
2021-04-14 15:38:52 +0800

    PartProgramPage: add tab bar and fab

--- lib/views/part_program_page.dart
+++ lib/views/part_program_page.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PartProgramPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Part Program'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Part Program'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(
+                child: Column(
+                  children: [Text('Inventory &'), Text('Warehousing')],
+                ),
+              ),
+              Tab(
+                child: Column(
+                  children: [Text('Ketersediaan'), Text('Part')],
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.save_outlined,
+            color: Color(0xffffcd29),
+          ),
+          onPressed: showDialog,
+        ),
       ),
-      body: Container(),
+    );
+  }
+
+  showDialog() {
+    return Get.defaultDialog(
+      radius: 17,
+      title: 'Simpan data',
+      middleText: 'Apakah data yang anda masukkan sudah benar?',
+      textConfirm: 'YA',
+      textCancel: 'Tidak',
+      confirmTextColor: Colors.black87,
+      buttonColor: Color(0xffffcd29),
+      cancelTextColor: Colors.black87,
+      onConfirm: () {
+        // controller.saveData();
+      },
+      onCancel: () => Get.toNamed('/part_program'),
     );
   }
 }

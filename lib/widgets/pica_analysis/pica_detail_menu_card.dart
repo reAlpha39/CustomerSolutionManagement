ee0f73c1e350a18af72b4ad4e363848c312ae650
ReAlpha39 <yunusw718@gmail.com>
2021-04-22 23:49:36 +0800

    pica_menu_card: show progress bar when the data still being load and fix title card overflowed

--- lib/widgets/pica_analysis/pica_detail_menu_card.dart
+++ lib/widgets/pica_analysis/pica_detail_menu_card.dart
@@ -12,171 +12,197 @@ class PicaDetailMenuCard extends StatelessWidget {
   final MsppController msppController = Get.find();
   @override
   Widget build(BuildContext context) {
-    picaCTController.mappedCard();
+    picaCTController.loadData();
     return Card(
       color: Color(0xffffcd29),
       margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 72.0),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
-      child: Column(
-        children: [
-          Container(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () => picaController.panelController.close(),
-                      child: Icon(
-                        LineIcons.times,
-                        color: Colors.black87,
-                      ),
+      child: Obx(
+        () => picaCTController.picaData.value.picaElement == null
+            ? Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.white,
+                ),
+              )
+            : Column(
+                children: [
+                  Container(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            TextButton(
+                              onPressed: () =>
+                                  picaController.panelController.close(),
+                              child: Icon(
+                                LineIcons.times,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            Obx(
+                              () => Container(
+                                width: 220,
+                                child: Text(
+                                  picaCTController
+                                      .picaData
+                                      .value
+                                      .picaElement[
+                                          picaController.indexDetailData.value]
+                                      .title,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                              border: Border.all(color: Colors.black26),
+                            ),
+                            child: Center(
+                                child: Obx(
+                              () => Text(
+                                picaCTController
+                                    .picaData
+                                    .value
+                                    .picaElement[
+                                        picaController.indexDetailData.value]
+                                    .score
+                                    .toString(),
+                                style: TextStyle(color: Colors.black87),
+                              ),
+                            )),
+                          ),
+                        ),
+                      ],
                     ),
-                    Obx(
-                      () => Text(
-                        picaCTController
-                            .picaData
-                            .value
-                            .picaElement[picaController.indexDetailData.value]
-                            .title,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+                  ),
+                  Flexible(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      child: Container(
+                        height: context.height,
+                        child: SingleChildScrollView(
+                          physics: BouncingScrollPhysics(),
+                          child: Obx(
+                            () => picaCTController.picaData.value.picaElement ==
+                                    null
+                                ? CircularProgressIndicator()
+                                : Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List<Widget>.generate(
+                                      picaCTController
+                                          .picaData
+                                          .value
+                                          .picaElement[picaController
+                                              .indexDetailData.value]
+                                          .picaDetail
+                                          .length,
+                                      (index) => Obx(
+                                        () => !msppController
+                                                .radioIndexPI(picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .id)
+                                                .contains(1)
+                                            ? Container()
+                                            : PicaDetailCard(
+                                                title: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .title,
+                                                id: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .id,
+                                                indexData: picaController
+                                                    .indexDetailData.value,
+                                                indexCard: index,
+                                                tag: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .id,
+                                                docA: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .tablePath
+                                                    .docA,
+                                                colA: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .tablePath
+                                                    .colA,
+                                                docB: picaCTController
+                                                    .picaData
+                                                    .value
+                                                    .picaElement[picaController
+                                                        .indexDetailData.value]
+                                                    .picaDetail[index]
+                                                    .tablePath
+                                                    .docB,
+                                                dataTableFilter: true,
+                                                dataTableListRadio: msppController
+                                                    .radioIndexPI(picaCTController
+                                                        .picaData
+                                                        .value
+                                                        .picaElement[
+                                                            picaController
+                                                                .indexDetailData
+                                                                .value]
+                                                        .picaDetail[index]
+                                                        .id),
+                                                dataTableRadioIndex: 1,
+                                                radioIndexA: picaCTController
+                                                    .indexResultA,
+                                                radioIndexB: picaCTController
+                                                    .indexResultB,
+                                              ),
+                                      ),
+                                    ),
+                                  ),
+                          ),
                         ),
                       ),
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black26),
-                    ),
-                    child: Center(
-                        child: Obx(
-                      () => Text(
-                        picaCTController
-                            .picaData
-                            .value
-                            .picaElement[picaController.indexDetailData.value]
-                            .score
-                            .toString(),
-                        style: TextStyle(color: Colors.black87),
-                      ),
-                    )),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(17),
-                ),
+                ],
               ),
-              child: Container(
-                height: context.height,
-                child: SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: List<Widget>.generate(
-                      picaCTController
-                          .picaData
-                          .value
-                          .picaElement[picaController.indexDetailData.value]
-                          .picaDetail
-                          .length,
-                      (index) => Obx(
-                        () => !msppController
-                                .radioIndexPI(picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .id)
-                                .contains(1)
-                            ? Container()
-                            : PicaDetailCard(
-                                title: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .title,
-                                id: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .id,
-                                indexData: picaController.indexDetailData.value,
-                                indexCard: index,
-                                tag: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .id,
-                                docA: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .tablePath
-                                    .docA,
-                                colA: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .tablePath
-                                    .colA,
-                                docB: picaCTController
-                                    .picaData
-                                    .value
-                                    .picaElement[
-                                        picaController.indexDetailData.value]
-                                    .picaDetail[index]
-                                    .tablePath
-                                    .docB,
-                                dataTableFilter: true,
-                                dataTableListRadio: msppController.radioIndexPI(
-                                    picaCTController
-                                        .picaData
-                                        .value
-                                        .picaElement[picaController
-                                            .indexDetailData.value]
-                                        .picaDetail[index]
-                                        .id),
-                                dataTableRadioIndex: 1,
-                                radioIndexA: picaCTController.indexResultA,
-                                radioIndexB: picaCTController.indexResultB,
-                              ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }

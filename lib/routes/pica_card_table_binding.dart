d357f4d911ba6e8b919f9eef7e8965e7fd04f75a
ReAlpha39 <yunusw718@gmail.com>
2021-04-22 23:42:54 +0800

    pica_card_binding: add remaining mspp tag

--- lib/routes/pica_card_table_binding.dart
+++ lib/routes/pica_card_table_binding.dart
@@ -14,5 +14,42 @@ class PicaCardTableBinding extends Bindings {
     Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'bspsp');
     Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'rcpsp');
     Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ade');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'pps');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'appt');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'epss');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'tpsnp');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'hptd');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'eds');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'rpll');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ctst');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'mtdt');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'field');
+    Get.put<PicaCardTableController>(PicaCardTableController(),
+        tag: 'workshop');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'km');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ltpp');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'cccd');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'opop');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'bsop');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'rcop');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ame');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'pl');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ar');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'nt');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'tr');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'hps');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'ej');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 're');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'to');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'iuip');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'gar');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'edr');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'rj');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'plcbm');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'arcbm');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'tacbm');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'evcbm');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'aea');
+    Get.put<PicaCardTableController>(PicaCardTableController(), tag: 'aeb');
   }
 }

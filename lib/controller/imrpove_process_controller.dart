7651e3de1f3b4941580532ebebcbeda6221cac90
ReAlpha39 <yunusw718@gmail.com>
2021-04-25 16:10:29 +0800

    improve_panel_content: add image picker button and function to pick image

--- lib/controller/imrpove_process_controller.dart
+++ lib/controller/imrpove_process_controller.dart
@@ -1,12 +1,43 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class ImproveProcessController extends GetxController {
   PanelController panelController;
+  final _picker = ImagePicker();
+  RxBool isPicked = false.obs;
+  Rx<File> image;
 
   @override
   void onInit() {
     panelController = PanelController();
     super.onInit();
+  }
+
+  Future imageFromCamera() async {
+    isPicked.value = false;
+    var file = await _picker.getImage(source: ImageSource.camera);
+    if (file != null) {
+      image = Rx<File>(File(file.path));
+      image.refresh();
+      isPicked.value = true;
+    } else {
+      print('No image selected');
+    }
+  }
+
+  Future imageFromGallery() async {
+    isPicked.value = false;
+    var file =
+        await _picker.getImage(source: ImageSource.gallery, imageQuality: 50);
+    if (file != null) {
+      image = Rx<File>(File(file.path));
+      image.refresh();
+      isPicked.value = true;
+    } else {
+      print('No image selected');
+    }
   }
 }
--- lib/widgets/improve_process/improve_panel_content.dart
+++ lib/widgets/improve_process/improve_panel_content.dart
@@ -1,6 +1,10 @@
+import 'package:customer/controller/imrpove_process_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:line_icons/line_icons.dart';
 
 class ImprovePanelContent extends StatelessWidget {
+  final ImproveProcessController controller = Get.find();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,52 +32,108 @@ class ImprovePanelContent extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
                       border: Border.all(color: Colors.black38),
                       borderRadius: BorderRadius.circular(17),
                     ),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12),
-                        child: Text(
-                          'Photo Before Improvement',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black54,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
+                    child: Obx(
+                      () => !controller.isPicked.value
+                          ? Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 12),
+                                child: Text(
+                                  'Photo Before Improvement',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.file(controller.image.value),
+                            ),
                     ),
                   ),
                 ),
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      elevation: MaterialStateProperty.all<double>(0),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(17),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all<double>(0),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17),
+                          ),
+                        ),
+                      ),
+                      onPressed: () => controller.imageFromCamera(),
+                      child: Container(
+                        width: 90,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              LineIcons.camera,
+                              color: Color(0xff4c3d0c),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text(
+                                'Camera',
+                                style: TextStyle(
+                                  color: Color(0xff4c3d0c),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    onPressed: () {},
-                    child: Text('Camera'),
                   ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      elevation: MaterialStateProperty.all<double>(0),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(17),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all<double>(0),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17),
+                          ),
+                        ),
+                      ),
+                      onPressed: () => controller.imageFromGallery(),
+                      child: Container(
+                        width: 90,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              LineIcons.sdCard,
+                              color: Color(0xff4c3d0c),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text(
+                                'SD Card',
+                                style: TextStyle(
+                                  color: Color(0xff4c3d0c),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    onPressed: () {},
-                    child: Text('SD Card'),
                   ),
                 ],
               )

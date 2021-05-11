76defea01b65eee99a6f0b6bb13a162d37bcb91b
ReAlpha39 <yunusw718@gmail.com>
2021-05-11 13:38:27 +0800

    pubspec: change connectivity checker dependency

--- lib/utils/connectivity_checker.dart
+++ lib/utils/connectivity_checker.dart
@@ -1,12 +1,14 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:get/get.dart';
 
 Future<bool> connectivityChecker() async {
-  bool hasConnection = false;
-  hasConnection = await DataConnectionChecker().hasConnection.then((value) {
+  final bool hasConnection =
+      await InternetConnectionChecker().hasConnection.then((value) {
     if (!value) {
-      
+      if (Get.isDialogOpen) {
+        Get.back(closeOverlays: true);
+      }
       Get.defaultDialog(
           title: 'Koneksi Error',
           middleText: 'Periksa kembali koneksi internet anda',

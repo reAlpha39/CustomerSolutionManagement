b63b58860d37054caeefb266f9a82701f4c2b708
ReAlpha39 <yunusw718@gmail.com>
2021-05-15 13:54:41 +0800

    home_menu: remove some page from admin menu

--- lib/widgets/home/home_admin.dart
+++ lib/widgets/home/home_admin.dart
@@ -1,5 +1,4 @@
 import 'package:customer/widgets/home/home_card.dart';
-import 'package:customer/widgets/home/home_card_expanded.dart';
 import 'package:flutter/material.dart';
 
 class HomeAdmin extends StatelessWidget {
@@ -7,12 +6,6 @@ class HomeAdmin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        HomeCardExpanded(),
-        HomeCard(
-          route: '/other_program_admin',
-          imageAsset: 'assets/images/other_program.png',
-          titleText: "Other Program",
-        ),
         HomeCard(
           route: '/manage_account',
           imageAsset: 'assets/images/service_program.png',
